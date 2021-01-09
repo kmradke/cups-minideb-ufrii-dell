16ea8813f8648cebbda91378317a728af1fd7cf5
Kevin Radke <kmradke@gmail.com>
2021-01-09 14:07:39 -0600

    Update run_cups.sh
    Add some output messages

--- root/run_cups.sh
+++ root/run_cups.sh
@@ -3,6 +3,7 @@ set -e
 set -x
 
 if [ $(grep -ci $CUPSADMIN /etc/shadow) -eq 0 ]; then
+    echo "+++ Adding lpadmin user..."
     useradd -r -G lpadmin -M $CUPSADMIN 
 fi
 echo $CUPSADMIN:$CUPSPASSWORD | chpasswd
@@ -12,9 +13,14 @@ mkdir -p /services
 rm -rf /etc/cups/ppd
 ln -s /config/ppd /etc/cups
 if [ ! -f /config/printers.conf ]; then
+    echo "+++ Creating empty printer configuration..."
     touch /config/printers.conf
 fi
+echo "+++ Copying persisted printer configuration..."
 cp /config/printers.conf /etc/cups/printers.conf
 
+echo "+++ Launching printer updates in the background..."
 /root/printer-update.sh &
+
+echo "+++ Launching cupsd in the foreground..."
 exec /usr/sbin/cupsd -f
