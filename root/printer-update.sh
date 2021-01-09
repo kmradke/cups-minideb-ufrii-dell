a41cf9c1909a647457db9ff74c9a220edace3ad0
Kevin Radke <kmradke@gmail.com>
2021-01-09 14:03:48 -0600

    Update printer-update.sh
    Add some output

--- root/printer-update.sh
+++ root/printer-update.sh
@@ -2,8 +2,11 @@
 inotifywait -m -e close_write,moved_to,create /etc/cups | 
 while read -r directory events filename; do
 	if [ "$filename" = "printers.conf" ]; then
+	        echo "+++ Removing old Airprint service files..."
 		rm -rf /services/AirPrint-*.service
+		echo "+++ Generating airprint service files..."
 		/root/airprint-generate.py -d /services
+		echo "+++ Persisting printer configuration..."
 		cp /etc/cups/printers.conf /config/printers.conf
 	fi
 done
